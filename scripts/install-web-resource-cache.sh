2f3691514f1c307ebf06f2ca920cb4e01dec3e21
Jon Cram <jon@webignition.net>
2018-10-28 16:05:37 +0000

    Run web_resource_cache database migrations

--- scripts/install-web-resource-cache.sh
+++ scripts/install-web-resource-cache.sh
@@ -3,5 +3,8 @@
 git clone https://github.com/webignition/web-resource-cache.git
 cd web-resource-cache
 chmod +x ./bin/console
+
+./bin/console doctrine:migrations:migrate --no-interaction
+
 composer require symfony/web-server-bundle
 ./bin/console server:start
