da8044e19746a68f2da6ea0f4b26a4888f0b98b1
Jon Cram <jon@webignition.net>
2018-10-28 16:08:27 +0000

    Install composer deps before running db migrations

--- scripts/install-web-resource-cache.sh
+++ scripts/install-web-resource-cache.sh
@@ -4,6 +4,8 @@ git clone https://github.com/webignition/web-resource-cache.git
 cd web-resource-cache
 chmod +x ./bin/console
 
+composer install
+
 ./bin/console doctrine:migrations:migrate --no-interaction
 
 composer require symfony/web-server-bundle
